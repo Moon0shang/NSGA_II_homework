output wrong result

NSGA_II.py
@@ -16,10 +16,16 @@ class NSGA_II(object):
         self.x_range = x_range
         self.pop_num = pop_num
 
-    # def select(self,):
-    #     'choose the one to cross'
-    #     parent = None
-    #     return parent
+    def select(self,pop,Usort,dis,prob):
+        'choose the one to cross'
+        p = np.random.rand([pop.shape[0]])
+        pnum = np.zeros(len(p))
+        pnum[p < prob] = 1
+        num = np.sum(pnum)
+        
+
+        parent = None
+        return parent
     def cross_mutate(self, pop):
         cr_pop = self.cross(pop)
         new_pop = np.vstack((cr_pop, pop))
@@ -160,26 +166,26 @@ class NSGA_II(object):
 
             p = np.random.rand()
 
-            if p <= 0.5:
-                delta = np.power(2 * p, 1 / (self.yita_m + 1)) - 1
-            else:
-                delta = 1 - np.power(2 * (1 - p), 1 / (self.yita_m + 1))
-
-            offspring[i] = parent[i] + delta
-
-            if offspring[i] < x_low:
-                offspring[i] = x_low
-            elif offspring[i] > x_up:
-                offspring[i] = x_up
-
             # if p <= 0.5:
-            #     epsq = np.power(2 * p + (1 - 2 * p) * (1 - (parent[i] - x_low) / (x_up - x_low)),
-            #                     1 / (self.yita_m + 1))-1
+            #     delta = np.power(2 * p, 1 / (self.yita_m + 1)) - 1
             # else:
-            #     epsq = 1 - np.power(2 * (1 - p) + 2 * (p - 0.5) * (1 - (x_up - parent[i]) / (x_up - x_low)),
-            #                         1 / (self.yita_m + 1))
+            #     delta = 1 - np.power(2 * (1 - p), 1 / (self.yita_m + 1))
 
-            # offspring[i] = parent[i] + epsq * (x_up - parent[i])
+            # offspring[i] = parent[i] + delta
+
+            # if offspring[i] < x_low:
+            #     offspring[i] = x_low
+            # elif offspring[i] > x_up:
+            #     offspring[i] = x_up
+
+            if p <= 0.5:
+                epsq = np.power(2 * p + (1 - 2 * p) * (1 - (parent[i] - x_low) / (x_up - x_low)),
+                                1 / (self.yita_m + 1)) - 1
+            else:
+                epsq = 1 - np.power(2 * (1 - p) + 2 * (p - 0.5) * (1 - (x_up - parent[i]) / (x_up - x_low)),
+                                    1 / (self.yita_m + 1))
+
+            offspring[i] = parent[i] + epsq * (x_up - x_low)
 
             # if parent[i] < x_low or parent[i] > x_up:
             #     # if offspring[i] > x_up*10:
@@ -281,32 +287,24 @@ class NSGA_II(object):
         next_pop = np.ones([self.pop_num, new_pop.shape[1]])
 
         for i in range(len(Usort)):
-            num += len(Usort[i])
-            num_debug = num
-            if num > self.pop_num:
+
+            level_num = len(Usort[i])
+            if self.pop_num <= (level_num + num):
+                diff = self.pop_num - num
                 idx = np.argsort(dis[i])
-                diff = num - self.pop_num
                 stay = idx[-diff:]
                 stay_idx = []
                 for s in stay:
                     stay_idx.append(Usort[i][s])
-                num = self.pop_num
                 try:
-                    next_pop[(num - len(stay_idx)):num,
-                             :] = new_pop[stay_idx, :]
+                    next_pop[num:self.pop_num, :] = new_pop[stay_idx, :]
                 except:
-                    print('length:', len(stay_idx))
-                    print('diff:%d\tnum:%d\tpop:%d\tnew:%d' %
-                          (diff, num_debug,  self.pop_num, len(new_pop)))
-                    next_pop[(num - len(stay_idx)):num,
-                             :] = new_pop[stay_idx, :]
-
+                    print('num:%d,stay:%d' % (num, len(stay)))
+                    next_pop[num:self.pop_num, :] = new_pop[stay_idx, :]
                 break
             else:
                 stay_idx = Usort[i]
-                next_pop[(num - len(stay_idx)):num, :] = new_pop[stay_idx, :]
+                next_pop[num:(num + level_num)] = new_pop[stay_idx, :]
+            num += level_num
 
-            # next_pop = np.vstack((next_pop, new_pop[Usort[i], :]))
-
-            # print('stay:', stay_idx)
-        return next_pop  # [1:, :]
+        return next_pop

run.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from test_func import choose_fun
 from NSGA_II import NSGA_II
@@ -15,17 +16,17 @@ def run(pop, gen, x_range, dim, prob, yita, func):
     Usort, dis = NSGA.dis_sort(values, dim)
     for i in range(gen):
         # selected = NSGA.select()
-        cross_pop = NSGA.cross(pop)
+        # cross_pop = NSGA.cross(pop)
 
-        find_err(cross_pop, 'cross')
+        # find_err(cross_pop, 'cross')
 
-        mut_pop = NSGA.mutation(pop)
+        # mut_pop = NSGA.mutation(pop)
 
-        find_err(mut_pop, 'mutate')
+        # find_err(mut_pop, 'mutate')
 
         new_pop = NSGA.cross_mutate(pop)
 
-        find_err(new_pop, 'stack')
+        find_err(new_pop, 'stack-cross-mutate')
 
         # values = np.empty((pop_num, dim))
         values = choose_fun(func, var_in=new_pop)
@@ -47,6 +48,7 @@ def find_err(pop, explain):
     a1 = np.sum(jug, 1)
     a2 = np.sum(jug)
     if a2 > 0:
+        time.sleep(0.1)
         print('*****************%s errors*************************' % explain)
 
 
@@ -76,6 +78,7 @@ def init_population(pop_num, func):
 def visualize(func, pop, dim):
 
     values = choose_fun(func, var_in=pop)
+    # print(len(pop))
 
     if dim == 2:
         plt.scatter(values[:, 0], values[:, 1], color='b', marker='*')
@@ -87,15 +90,15 @@ def visualize(func, pop, dim):
 
 if __name__ == "__main__":
 
-    generation = 300
-    pop_num = 100
+    generation = 100
+    pop_num = 50
 
     prob_c = 0.9
     prob_m = 0.1
     prob = [prob_c, prob_m]
 
-    yita_c = 20
-    yita_m = 20
+    yita_c = 10
+    yita_m = 10
     yita = [yita_c, yita_m]
 
     func = ['ZDT_1', 'ZDT_2', 'ZDT_3', 'ZDT_4',

test_func.py
@@ -32,17 +32,24 @@ def ZDT_f1(infos, x):
         return x_range, m
     else:
         output = np.empty([len(x), 2])
-        f = x[:, 0]
-        g = 1 + 9 * np.sum(x[:, 1:], 1) / (m - 1)
-        h = 1 - np.sqrt(f / g)
+        # f = x[:, 0]
+        # g = 1 + 9 * np.sum(x[:, 1:], 1) / (m - 1)
+        # h = np.multiply(1 - np.sqrt(f / g), g)
+
+        for i in range(len(x)):
+            output[i, 0] = x[i, 0]
+            g = 1
+            for j in range(1, m):
+                g += 9 * x[i, j] / (m - 1)
+            output[i, 1] = g*(1-np.power(output[i, 0]/g, 0.5))
         # # fx = np.arange(x_range[0], x_range[1], 200)
         # # v_true = 1 - np.sqrt(fx / pof)
         # for i in range(len(x)):
         #     output[i, 0] = x[i, 0]
         #     g = 1 + 9 * np.sum(x[i, 1:]) / (m - 1)
         #     output[i, 1] = 1-np.sqrt(output[i, 0]/g)
-        output[:, 0] = f
-        output[:, 1] = h
+        # output[:, 0] = f
+        # output[:, 1] = h
         # output[:, 2] = v_true
 
         return output